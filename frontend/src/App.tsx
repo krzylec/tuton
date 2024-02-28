172473c363609ba7eea6dbd0fd0c0da851b8575c
Krzysiek <leczycki97@gmail.com>
2024-02-28 22:55:54 +0100

    tailwind2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import { Tldraw } from "@tldraw/tldraw";
 import './App.css';
+import './App.css'
 import Whiteboard from './WhiteBoard';
 import TabExplorer from './TabExplorer';
 
 function App() {
   return (
     <>
-      <h1>tuton</h1>
+      <h1 className="text-center mb-2 text-3xl font-bold hover:animate-spin">
+        TUTORN
+      </h1>
       <TabExplorer />
     </>
   );
