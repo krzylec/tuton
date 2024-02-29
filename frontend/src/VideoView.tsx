ec348fa758fe810c8ffcb6e1b1fdc52fcb32512d
Krzysiek <leczycki97@gmail.com>
2024-02-29 22:04:32 +0100

    resposive flashcard

--- frontend/src/VideoView.tsx
+++ frontend/src/VideoView.tsx
@@ -1,30 +1,37 @@
-import Fiszka from "./Flashcard"
-import Player from "./Player"
-import Whiteboard from "./WhiteBoard"
+import Fiszka from "./Flashcard";
+import Player from "./Player";
+import Whiteboard from "./WhiteBoard";
 
 export default function VideoView() {
+  return (
+    <div className="flex flex-col items-center justify-center bg-gray-100">
+      <div className="w-full bg-blue-500 justify-center flex pb-10 pt-5">
+        <Player />
+      </div>
 
-    return (
-
-        <div className="flex flex-col items-center justify-center bg-gray-100">
-
-            <div className="w-full bg-blue-500 justify-center flex pb-10 pt-5">
-                <Player         />
-            </div>
-
-            <div className="flex flex-wrap justify-center">
-                <div className="flex flex-nowrap">
-                    {[1, 2, 3, 4].map((index) => (
-                        <Fiszka key={index} title={`Fiszka ${index}`} content={`Zawartość fiszki ${index}`} />
-                    ))}
-                </div>
-
-                <div className="flex flex-nowrap">
-                    {[5, 6, 7, 8].map((index) => (
-                        <Fiszka key={index} title={`Fiszka ${index}`} content={`Zawartość fiszki ${index}`} />
-                    ))}
-                </div>
-            </div>
+      <div className="flex flex-wrap justify-center">
+        <div className="flex flex-nowrap">
+          {[1, 2, 3, 4].map((index) => (
+            <Fiszka
+              key={index}
+              title={`Fiszka ${index}`}
+              content={`Zawartość fiszki ${index}`}
+              contentFlipped={`Zawartość fiszki zmieniona ${index}`}
+            />
+          ))}
         </div>
-    )
+
+        <div className="flex flex-nowrap">
+          {[5, 6, 7, 8].map((index) => (
+            <Fiszka
+              key={index}
+              title={`Fiszka ${index}`}
+              content={`Zawartość fiszki ${index}`}
+              contentFlipped={`Zawartość fiszki zmieniona ${index}`}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
