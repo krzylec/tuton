06a323ce09a95bed4edb33c2c098aaaaa6b9f49f
Kackan1 <Kacper.Kaniuka@wp.pl>
2024-03-01 22:49:10 +0100

    dalsza konfiguracja springa

--- backend/src/main/java/com/tuton/backend/BackendApplication.java
+++ backend/src/main/java/com/tuton/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.tuton;
+package com.tuton.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TutonApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TutonApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/tuton/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/tuton/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.tuton;
+package com.tuton.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TutonApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
