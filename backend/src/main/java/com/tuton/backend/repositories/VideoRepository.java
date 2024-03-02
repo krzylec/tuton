735a64e9960cc08fedd1a369ccbde55b1c8f9e30
Krzysiek <leczycki97@gmail.com>
2024-03-02 15:56:57 +0100

    todo, dobrej zabawy

---
+++ backend/src/main/java/com/tuton/backend/repositories/VideoRepository.java
@@ -0,0 +1,10 @@
+package com.tuton.backend.repositories;
+
+import com.tuton.backend.model.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video, Long> {
+
+}
